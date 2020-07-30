6ee0a0727b1778371a539cf64f479669dbb63022
Nauman Ahmad <nauman2077@gmail.com>
2020-07-30 13:42:18 +0500

    Initialize project using Create React App

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul class="navbar">
+          <li>
+            <Link to="/">Pic Some</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <i className="ri-shopping-cart-line ri-2x"></i>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Photos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Photos() {
+    return (
+        <div>
+            <h1>Photos component</h1>
+        </div>
+    )
+}
+
+export default Photos;
